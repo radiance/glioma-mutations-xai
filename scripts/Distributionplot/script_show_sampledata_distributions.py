247bdda791862de4ab1fd8a04019bced2864e1ce
radiance <radiance@users.noreply.github.com>
2020-10-05 19:16:13 +0200

    update to distribution plots

--- scripts/Distributionplot/script_show_sampledata_distributions.py
+++ scripts/Distributionplot/script_show_sampledata_distributions.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_frame = pd.read_csv("..\data\mutations_merged_filtered_and_processed.csv", sep=';')
+dim_list = ['Cancer Study', 'Age', 'Mutation_Count']
+
+data_frame = pd.read_csv("..\data\mutations_merged_filtered_and_processed.csv", sep=';') #usecols=dim_list,
 
 print('\n-----------------------')
 print('showing age distribution')
@@ -13,7 +15,14 @@ plt.show()
 
 print('\n-----------------------')
 print('showing mutation count distribution')
-ax = data_frame[['DifferentMutatedGenesCount']].plot.hist(bins=100,legend=False) #Mutation_Count,DifferentMutatedGenesCount
-ax.set_xlabel("mutationcount per age")
+#data_frame.sort_values(by='Age', ascending=True)
+ax = data_frame.plot('Age','Mutation_Count') #Mutation_Count,DifferentMutatedGenesCount
+plt.tight_layout()
+plt.show()
+
+print('\n-----------------------')
+print('showing samples per study distribution')
+ax = data_frame['Cancer Study'].value_counts().plot.bar(legend=True) #Mutation_Count,DifferentMutatedGenesCount
 ax.set_ylabel("# samples")
+plt.tight_layout()
 plt.show()
